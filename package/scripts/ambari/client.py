a8097b8b414965a95832b039ce2184650c2b39fb
Francois Dion <francois.dion@gmail.com>
2015-07-16 14:35:42 -0400

    Cleanup of python scripts

--- package/scripts/ambari/client.py
+++ package/scripts/ambari/client.py
@@ -1,23 +1,27 @@
-import sys, os, pwd, grp, signal
-from resource_management import *
 import ambari_helpers as helpers
+from resource_management import ClientComponentHasNoStatus, Execute, Script
+
 
 class Client(Script):
-  def install(self, env):
-    self.configure(env)
-    import client_params as params
+    def install(self, env):
+        self.configure(env)
+        import client_params as params
 
-    # Add repos to yum
-    helpers.add_repos()
-    self.install_packages(env)
+        # Add repos to yum
+        helpers.add_repos()
+        self.install_packages(env)
 
-    for command in params.commands: Execute(command)
+        for command in params.commands:
+            Execute(command)
 
-  def status(self, env): raise ClientComponentHasNoStatus()
+    def status(self, env):
+        raise ClientComponentHasNoStatus()
 
-  def configure(self, env):
-    import client_params as params
-    env.set_params(params)
+    def configure(self, env):
+        import client_params as params
+
+        env.set_params(params)
+
 
 if __name__ == "__main__":
-  Client().execute()
+    Client().execute()
--- package/scripts/ambari/master_params.py
+++ package/scripts/ambari/master_params.py
@@ -17,7 +17,7 @@ spark_home = config['configurations']['jupyter-env']['spark.home']
 package_dir = helpers.package_dir()
 files_dir = package_dir + 'files/'
 scripts_dir = package_dir + 'scripts/'
-root_dir = '/home/'+user
+root_dir = '/home/' + user
 
 linux_users = [user]
 hdfs_users = [user]
@@ -25,16 +25,18 @@ hdfs_users = [user]
 # Commands executed in order in master.py
 commands = []
 # First create all needed dirs
-mkdirs=[root_dir, root_dir+'/notebooks', pid_dir, log_dir, root_dir+'/.ipython']
+mkdirs = [root_dir, root_dir + '/notebooks', pid_dir, log_dir, root_dir + '/.ipython']
 for dir in mkdirs: commands.append('mkdir -p ' + dir)
 
 # Jupyter config script
-commands.append(' '.join(['sh', scripts_dir+'shell/jupyter_setup.sh', root_dir, files_dir, spark_home, jupyter_port]))
+commands.append(' '.join(['sh', scripts_dir + 'shell/jupyter_setup.sh', root_dir, files_dir, spark_home, jupyter_port]))
 
 # Set ownership of all directories to proper users
 for dir in mkdirs:
-  commands.append('chown -R ' + user + ' ' + dir)
-  commands.append('chgrp -R ' + user + ' ' + dir)
+    commands.append('chown -R ' + user + ' ' + dir)
+    commands.append('chgrp -R ' + user + ' ' + dir)
 
-start_args = ['"ipython notebook --profile default --ipython-dir '+root_dir+'/.ipython --notebook-dir '+root_dir+'/notebooks"', log_dir + '/server.log', pid_file]
+start_args = [
+    '"ipython notebook --profile default --ipython-dir ' + root_dir + '/.ipython --notebook-dir ' + root_dir + '/notebooks"',
+    log_dir + '/server.log', pid_file]
 start_command = 'source ~/.bashrc; sh ' + scripts_dir + 'shell/start.sh ' + ' '.join(start_args)
